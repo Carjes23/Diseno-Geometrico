17f51785d9a27ccf203c03f5e57f73d1dc965906
mjuribe <105825647+mjuribe@users.noreply.github.com>
2022-06-19 20:48:06 -0500

    Estilo de las graficas interactivas

--- Interactivo.py
+++ Interactivo.py
@@ -6,6 +6,7 @@ import numpy as np
 from bsplines import basisFunction
 from boor import Boor
 from draggable_plot import * 
+
 def calculaT(Q):
   m = len(Q) - 1
   T = [0.0 for i in range(m+1)]
@@ -75,8 +76,10 @@ def calcularInvNTNyNT(l, p, T, m, U, n):
     invNTN = np.linalg.inv(NTN)
 
     return [invNTN, NT]
-p = int(input("De el grado: "))
-n = int(input("Indice mayor de puntos de contro: "))
+
+p = int(input("Dé el grado del B-Spline: "))
+n = int(input("Índice mayor de puntos de control: "))
+
 if __name__ == "__main__":
     plot = DraggablePlotExample()
     dictionary = plot.get_points()
@@ -119,11 +122,15 @@ for i in range(len(P)): #implementación sin derivadas
     for j in range(len(P[i])):
       P[i][j] = Pi[contElement][j]
     contElement += 1
+
 grado = p
-U = U[p:-p] #Reducción puntos repeditos
+U = U[p:-p] #Reducción puntos repetidos
+
 (X, Y) = Boor(grado, P, U)
-plt.scatter(P[:,0], P[:,1],marker='o', color = 'black')
-plt.scatter(Q[:,0], Q[:,1],marker='X', color = 'green')
-plt.plot(X,Y, color='purple')
+plt.scatter(P[:,0], P[:,1],marker='o', color = 'black', label="Puntos de control") 
+plt.scatter(Q[:,0], Q[:,1],marker='X', color = 'green', label="Datos Q") 
+plt.plot(X,Y, color='purple', label="Curva B-spline") 
+plt.legend(loc="best")
+plt.title("Curva B-spline")
 plt.show()
 
--- draggable_plot.py
+++ draggable_plot.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-
 import math
-
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseEvent
 
-
 class DraggablePlotExample(object):
-    u""" An example of plot with draggable markers """
+    u""" Un ejemplo de una gráfica con marcadores arrastrables """
 
     def __init__(self):
         self._figure, self._axes, self._line = None, None, None
@@ -17,7 +13,7 @@ class DraggablePlotExample(object):
         self._init_plot()
 
     def _init_plot(self):
-        self._figure = plt.figure("Example plot")
+        self._figure = plt.figure("Gráfica de datos Q")
         axes = plt.subplot(1, 1, 1)
         axes.set_xlim(-25, 25)
         axes.set_ylim(-25, 25)
@@ -34,10 +30,10 @@ class DraggablePlotExample(object):
             self._line.set_data([], [])
         else:
             x, y = zip(*sorted(self._points.items()))
-            # Add new plot
+            # Añade nueva gráfica
             if not self._line:
-                self._line, = self._axes.plot(x, y, "b", marker="o", markersize=10)
-            # Update current plot
+                self._line, = self._axes.plot(x, y, color="C4", marker="o", markersize=5)
+            # Actualiza la gráfica actual
             else:
                 self._line.set_data(x, y)
         self._figure.canvas.draw()
@@ -54,10 +50,10 @@ class DraggablePlotExample(object):
             self._points.pop(x)
 
     def _find_neighbor_point(self, event):
-        u""" Find point around mouse position
+        u""" Encontrar puntos alrededor de la posición del mouse
 
         :rtype: ((int, int)|None)
-        :return: (x, y) if there are any point around mouse else None
+        :return: (x, y) if no hay ningún punto alrededor del mouse else None
         """
         distance_threshold = 3.0
         nearest_point = None
@@ -76,7 +72,7 @@ class DraggablePlotExample(object):
 
         :type event: MouseEvent
         """
-        # left click
+        # click izquierdo
         if event.button == 1 and event.inaxes in [self._axes]:
             point = self._find_neighbor_point(event)
             if point:
@@ -84,7 +80,7 @@ class DraggablePlotExample(object):
             else:
                 self._add_point(event)
             self._update_plot()
-        # right click
+        # click derecho
         elif event.button == 3 and event.inaxes in [self._axes]:
             point = self._find_neighbor_point(event)
             if point:
